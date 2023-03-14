5cc986fadf23df57fb78a2aa4925fcabfb6f0afe
Caution-Sun <hisbrjseka414>
2023-03-14 18:58:06 +0900

    Make Question Intent

--- app/src/main/java/org/techtown/q_self/MakeSbjQActivity.java
+++ app/src/main/java/org/techtown/q_self/MakeSbjQActivity.java
@@ -2,7 +2,10 @@ package org.techtown.q_self;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MakeSbjQActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MakeSbjQActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_makeq_sbj);
+
+        Button btn_CreateQ = findViewById(R.id.btn_createQ);
+
+        btn_CreateQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/org/techtown/q_self/SelectQActivity.java
+++ app/src/main/java/org/techtown/q_self/SelectQActivity.java
@@ -1,14 +1,52 @@
 package org.techtown.q_self;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SelectQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Toast.makeText(getApplicationContext(), "RESULT_OK", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectq);
+
+        Button btn_oa = findViewById(R.id.btn_oa);
+        Button btn_sa = findViewById(R.id.btn_sa);
+
+        btn_oa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MakeObjQActivity.class);
+                startActivityForResult(intent, 2);
+            }
+        });
+
+        btn_sa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MakeSbjQActivity.class);
+                startActivityForResult(intent, 3);
+            }
+        });
+
+
     }
 }
